321ce76ed573773e8fab65d127551775617a6e9b
slumboy <Taa_wappey@windowslive.com>
2016-09-29 11:09:10 +0700

    index -> category-icon

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -6,6 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>MSU Helping</title>
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
     {!! Html::style('css/bootstrap.min.css') !!}
     {!! Html::style('css/bootstrap.css') !!}
     {!! Html::style('css/w3.css') !!}
