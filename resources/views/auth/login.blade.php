f1e1e20fe917c815125e43e65b4bcb8b41879eb2
slumboy <Taa_wappey@windowslive.com>
2016-10-06 19:31:47 +0700

    complete

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,20 +1,22 @@
 @extends('layouts.app')
-<<<<<<< HEAD
 @section('contents')
     <style>
         @font-face {
             font-family: ThaiNeue;
             src: url('fonts/ThaiSansNeue-Light.ttf');
         }
-        body{
+
+        body {
             background-image: url("image/background.png");
         }
-        .panel{
+
+        .panel {
             margin-top: 20%;
             box-shadow: 1px 1px 50px #8c8c8c;
             border-top: 4px solid black;
         }
-        .panel-heading,label{
+
+        .panel-heading, label {
             font-family: ThaiNeue;
             font-size: 16pt;
         }
@@ -22,7 +24,8 @@
     <body>
     <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
     {!! Html::style('css/w3.css') !!}
-    <div class="container">
+    {!! Html::style('css/animate.css') !!}
+    <div class="container animated zoomIn">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
@@ -35,7 +38,7 @@
                                 <label for="email" class="col-md-4 control-label">อีเมล์</label>
                                 <div class="col-md-6">
                                     <input id="email" type="email" name="email" value="{{ old('email') }}" required
-                                           autofocus class="w3-input w3-animate-input" style="width:280px"><br>
+                                           class="w3-input w3-animate-input" style="width:280px"><br>
                                     @if ($errors->has('email'))
                                         <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
@@ -64,22 +67,20 @@
                                         <label>
                                             <input type="checkbox" name="remember"> Remember Me
                                         </label>
+                                        <button type="submit" class="w3-btn w3-hover-blue" style="font-family: ThaiNeue;font-size: 16pt;margin-left: 25%">
+                                            เข้าสู่ระบบ
+                                        </button>
                                     </div>
                                 </div>
                             </div>
 
                             <div class="form-group">
-                                <div class="col-md-8 col-md-offset-4">
-                                    <button type="submit" class="w3-btn w3-hover-teal">
-                                        Login
-                                    </button>
-
-                                    <button type="submit" class="w3-btn w3-indigo w3-hover-blue">
-                                        Facebook
-                                    </button>
-
+                                <div class="col-md-8 col-md-offset-4" style="margin-left: 30%">
                                     <button type="submit" class="w3-btn w3-red w3-hover-pink">
-                                        Google+
+                                        <i class="fa fa-google-plus">&nbsp;&nbsp;|&nbsp;Sign in Google+</i>
+                                    </button>
+                                    <button type="submit" class="w3-btn w3-indigo w3-hover-blue">
+                                        <i class="fa fa-facebook">&nbsp;&nbsp;|&nbsp;Sign in Facebook</i>
                                     </button>
                                 </div>
                             </div>
@@ -92,77 +93,7 @@
                             </div>
                         </form>
                     </div>
-=======
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember"> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ url('/password/reset') }}">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
-                        </div>
-                    </form>
->>>>>>> origin/master
+                    {{--Script--}}
                 </div>
-            </div>
-        </div>
-    </div>
-<<<<<<< HEAD
 @endsection
-{{--Script--}}
-=======
-</div>
-@endsection
->>>>>>> origin/master
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,21 +1,22 @@
 @extends('layouts.app')
-
-<<<<<<< HEAD
 @section('contents')
     <style>
         @font-face {
             font-family: ThaiNeue;
             src: url('fonts/ThaiSansNeue-Light.ttf');
         }
-        body{
+
+        body {
             background-image: url("image/background.png");
         }
-        .panel{
+
+        .panel {
             margin-top: 20%;
             box-shadow: 1px 1px 50px #8c8c8c;
             border-top: 4px solid black;
         }
-        .panel-heading,label{
+
+        .panel-heading, label{
             font-family: ThaiNeue;
             font-size: 16pt;
         }
@@ -23,20 +24,22 @@
     <body>
     <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
     {!! Html::style('css/w3.css') !!}
-        <div class="container">
+    {!! Html::style('css/animate.css') !!}
+    <div class="container animated zoomIn">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
                     <div class="panel-heading">สมัครสมาชิก <i class="fa fa-user-plus"></i></div>
                     <div class="panel-body">
-                        <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
+                        <form class="form-horizontal " role="form" method="POST" action="{{ url('/register') }}">
                             {{ csrf_field() }}
 
                             <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
                                 <label for="name" class="col-md-4 control-label">ชื่อ - นามสกุล</label>
 
                                 <div class="col-md-6">
-                                    <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+                                    <input id="name" type="text" class="form-control" name="name"
+                                           value="{{ old('name') }}" required autofocus>
 
                                     @if ($errors->has('name'))
                                         <span class="help-block">
@@ -50,7 +53,8 @@
                                 <label for="email" class="col-md-4 control-label">อีเมล์</label>
 
                                 <div class="col-md-6">
-                                    <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                                    <input id="email" type="email" class="form-control" name="email"
+                                           value="{{ old('email') }}" required>
 
                                     @if ($errors->has('email'))
                                         <span class="help-block">
@@ -78,7 +82,8 @@
                                 <label for="password-confirm" class="col-md-4 control-label">ยืนยันรหัสผ่าน</label>
 
                                 <div class="col-md-6">
-                                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                                    <input id="password-confirm" type="password" class="form-control"
+                                           name="password_confirmation" required>
 
                                     @if ($errors->has('password_confirmation'))
                                         <span class="help-block">
@@ -90,96 +95,11 @@
 
                             <div class="form-group">
                                 <div class="col-md-6 col-md-offset-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        Register
+                                    <button type="submit" class="w3-btn w3-hover-blue animated zoomIn" style="font-family: ThaiNeue;font-size: 16pt;margin-left: 100px">
+                                        สมัครสมาชิก
                                     </button>
                                 </div>
                             </div>
                         </form>
                     </div>
-=======
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
-
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-
-                                @if ($errors->has('password_confirmation'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password_confirmation') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Register
-                                </button>
-                            </div>
-                        </div>
-                    </form>
->>>>>>> origin/master
-                </div>
-            </div>
-        </div>
-    </div>
-<<<<<<< HEAD
-    </body>
-=======
-</div>
->>>>>>> origin/master
 @endsection
