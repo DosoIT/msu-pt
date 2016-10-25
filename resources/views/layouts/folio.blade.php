8aa9fdd53233aef39eae26481e7c838de4910fa8
slumboy <Taa_wappey@windowslive.com>
2016-10-25 16:13:27 +0700

    ok comit

--- resources/views/layouts/folio.blade.php
+++ resources/views/layouts/folio.blade.php
@@ -11,7 +11,8 @@
 <div class="container">
     <div class="row">
         <div class="col-xs-12">
-            <h2 style="font-family: 'ThaiNeue'; font-size: 48px">รับสมัครงาน <span style="font-size: 14pt;" class="w3-tag w3-teal">งานใหม่ในรอบสัปดาห์!</span>
+            <h2 style="font-family: 'ThaiNeue'; font-size: 48px">รับสมัครงาน <span style="font-size: 14pt;"
+                                                                                   class="w3-tag w3-teal">งานใหม่ในรอบสัปดาห์!</span>
             </h2>
             <h2 style="font-family: 'ThaiNeue'; font-size: 26pt;margin:40px 30px 0px 0px" class="w3-display-topright">
                 <a href="#" class="show-all" title="'คลิก' ดูทั้งหมด">ทั้งหมด >></a>
@@ -22,75 +23,28 @@
         <div class="table-responsive">
             <table class="table table-hover">
                 <tbody>
+                <?php
+                $mysqli = mysqli_connect('localhost', 'root', '', 'msu_pt') or die('Connect DB Error');
+                mysqli_set_charset($mysqli,'utf8');
+                $query = "SELECT * FROM work_posts";
+                $result = mysqli_query($mysqli, $query);
+                while ($row = mysqli_fetch_array($result)){
+                ?>
                 <tr>
                     <td class="td-img">
-                        <img src="image/pic-default.png" alt="Person" class="w3-card-4 news-post">
+                        <img src="picture/<?php echo $row['wp_pic'] ?>" alt="Person" class="w3-card-4 news-post">
                     </td>
+
                     <td width="900">
-                        <p><a href=""><b>บริษัทไอทีคอร์ป จำกัด</b></a></p>
-                        <p style="color: red">(1 ตำแหน่ง) </p>
-                        <p>Open House 19 ตุลานี้!!! รับทุกสาขา ทุกตำแหน่ง ยินดีรับผู้สมัครไม่มีประสบการณ์ ,
-                            ผู้จัดการสาขา Mezzo JobFair เซ็นทรัลรัตนาธิเบศน์ สมัคร 21 ตค. ทราบผล24ชม.,
-                            ผู้ช่วยผู้จัดการฝ่ายฝึกอบรม Barista (ติดต่อ..</p>
+                        <p><a href=""><b><?php echo $row['wp_titel']; ?></b></a></p>
+                        <p style="color: red"><?php echo "(&nbsp;".$row['wp_total']."&nbsp;ตำแหน่ง)"; ?></p>
+                        <p><?php echo $row['wp_detail']; ?></p>
                     </td>
                     <td>
-                        <p style="color: #cccccc">วันที่ 16 ตุลาคม 2559</p>
+                        <p style="color: #cccccc"><?php echo $row['created_at']; ?></p>
                     </td>
                 </tr>
-                <tr>
-                    <td class="td-img">
-                        <img src="image/pic-default.png" alt="Person" class="w3-card-4 news-post">
-                    </td>
-                    <td>
-                        <p><a href=""><b>บริษัท คิงส์ เทเลคอม จำกัด</b></a></p>
-                        <p style="color: red">(5 ตำแหน่ง) </p>
-                        <p>HR Manager (บางนา/ชลบุรี), Internal Audit, เจ้าหน้าที่ควบคุมและจัดการโครงการ, Sales
-                            Engineer, เจ้าหน้าที่ธุรการจัดซื้อ..</p>
-                    </td>
-                    <td>
-                        <p style="color: #cccccc">วันที่ 16 ตุลาคม 2559</p>
-                    </td>
-                </tr>
-                <tr>
-                    <td class="td-img">
-                        <img src="image/pic-default.png" alt="Person" class="w3-card-4 news-post">
-                    </td>
-                    <td>
-                        <p><a href=""><b>ร้าน A.Ns cezz</b></a></p>
-                        <p style="color: red">(1 ตำแหน่ง) </p>
-                        <p>พนักงานทั่วไป..</p>
-                    </td>
-                    <td>
-                        <p style="color: #cccccc">วันที่ 16 ตุลาคม 2559</p>
-                    </td>
-                </tr>
-                <tr>
-                    <td class="td-img">
-                        <img src="image/pic-default.png" alt="Person" class="w3-card-4 news-post">
-                    </td>
-                    <td>
-                        <p><a href=""><b>Ultimate Technology Co., Ltd.</b></a></p>
-                        <p style="color: red">(3 ตำแหน่ง) </p>
-                        <p>พนักงานตรวจสอบสัญญาณโทรศัพท์, เจ้าหน้าที่ดูแลคลังสินค้า, Mobile Application Developer (iOS,
-                            Android)..</p>
-                    </td>
-                    <td>
-                        <p style="color: #cccccc">วันที่ 16 ตุลาคม 2559</p>
-                    </td>
-                </tr>
-                <tr>
-                    <td class="td-img">
-                        <img src="image/pic-default.png" alt="Person" class="w3-card-4 news-post">
-                    </td>
-                    <td>
-                        <p><a href=""><b>PBC Develop Corporation Co., Ltd.</b></a></p>
-                        <p style="color: red">(1 ตำแหน่ง) </p>
-                        <p>หัวหน้าแผนกบัญชีการเงิน..</p>
-                    </td>
-                    <td>
-                        <p style="color: #cccccc">วันที่ 16 ตุลาคม 2559</p>
-                    </td>
-                </tr>
+                <?php }?>
                 </tbody>
             </table>
         </div>
