daedd760521a3cda56af75c358daf314ce7cbbbd
Jeremylinkk <Siriwut-77@hotmail.com>
2016-10-29 16:10:49 +0700

    model

--- resources/views/layouts/folio.blade.php
+++ resources/views/layouts/folio.blade.php
@@ -25,7 +25,7 @@
                 <tbody>
                 <?php
                 $mysqli = mysqli_connect('localhost', 'root', '', 'msu_pt') or die('Connect DB Error');
-                mysqli_set_charset($mysqli,'utf8');
+                mysqli_set_charset($mysqli, 'utf8');
                 $query = "SELECT * FROM work_posts";
                 $result = mysqli_query($mysqli, $query);
                 while ($row = mysqli_fetch_array($result)){
@@ -34,12 +34,27 @@
                     <td class="td-img">
                         <img src="picture/<?php echo $row['wp_pic'] ?>" alt="Person" class="w3-card-4 news-post">
                     </td>
-
                     <td width="900">
-                        <p><a href=""><b><?php echo $row['wp_titel']; ?></b></a></p>
-                        <p style="color: red"><?php echo "(&nbsp;".$row['wp_total']."&nbsp;ตำแหน่ง)"; ?></p>
+                        <p>
+                            <a href="{{ route('showpostEmployer.show',$row['wp_id'] ) }}"
+                               data-toggle="modal" data-target="#detail<?php echo $row['wp_id']?>">
+                                {{ method_field('GET') }}
+                                {{ csrf_field() }}
+                                <b><?php echo $row['wp_titel']; ?></b>
+                            </a>
+                        </p>
+                        <p style="color: red"><?php echo "(&nbsp;" . $row['wp_total'] . "&nbsp;ตำแหน่ง)"; ?></p>
                         <p><?php echo $row['wp_detail']; ?></p>
                     </td>
+                    {{--model--}}
+                    <div class="modal fade" id="detail<?php echo $row['wp_id']?>" tabindex="-1"
+                         role="dialog" aria-labelledby="myModalLabel">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                            </div>
+                        </div>
+                    </div>
+                    {{--end model--}}
                     <td>
                         <p style="color: #cccccc"><?php echo $row['created_at']; ?></p>
                     </td>
--- resources/views/lin/detail_post.blade.php
+++ resources/views/lin/detail_post.blade.php
@@ -4,102 +4,142 @@
         font-size: 20pt;
         margin-left: 1cm;
     }
+
+    .ul-fix > li {
+        float: left;
+        margin-top: 1%;
+        margin-right: 2%;
+        list-style: none;
+        font-family: ThaiNeue;
+        font-size: 16pt;
+    }
+
+    .bt1 > p {
+        font-size: 22pt;
+        margin-top: -10px;
+    }
+
+    .li-icon > i {
+        margin: 0px 10px 0px 10px;
+    }
 </style>
-<div class="modal-header">
+<div class="modal-header" style="heigth: 50px; ">
     <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
                 aria-hidden="true">&times;</span></button>
-    <h4 class="modal-title fonts" id="myModalLabel">รายละเอียด</h4>
+    {{--<h5 class="modal-title fonts" id="myModalLabel">รายละเอียด</h5>--}}
+    <p class="modal-title fonts" id="myModalLabel">รายละเอียด</p>
 </div>
-<div class="modal-body row" style="margin-top: -1cm;">
-    <div class="table-responsive">
-        <table class="table table-hover table-condensed" cellpadding="0" cellspacing="0">
-            <tr class="fonts">
-                <td width="30%" align="right">
-                    ชื่อบริษัท/หัวข้อ :
-                </td>
-                <td>
-                    @foreach($detail_post as $values){{ $values->wp_titel }}@endforeach
-                </td>
-            </tr>
-            <tr class="fonts" >
-                <td width="30%" align="right">
-                    เบอร์โทร :
-                </td>
-                <td class="fonts">
-                    @foreach($detail_post as $values){{ $values->wp_tel }}@endforeach
-                </td>
-            </tr>
-            <tr  class="fonts">
-                <td width="30%" align="right">
-                    รายละเอียด :
-                </td>
-                <td class="fonts">
-                    @foreach($detail_post as $values){{ $values->wp_detail }}@endforeach
-                </td>
-            </tr>
-            <tr  class="fonts">
-                <td width="30%" align="right">
-                    ตำแหน่ง :
-                </td>
-                <td class="fonts">
-                    @foreach($detail_post as $values){{ $values->wp_location }}@endforeach
-                </td>
-            </tr>
-            <tr  class="fonts">
-                <td width="30%" align="right">
-                    ประเภทงาน :
-                </td>
-                <td class="fonts">
-                    @foreach($detail_post as $values){{ $values->wp_description }}@endforeach
-                </td>
-            </tr>
-            <tr  class="fonts">
-                <td width="30%" align="right">
-                    คุณสมบัติ :
-                </td>
-                <td class="fonts">
-                    @foreach($detail_post as $values){{ $values->wp_property }}@endforeach
-                </td>
-            </tr>
-            <tr  class="fonts">
-                <td width="30%" align="right">
-                    เบอร์โทร :
-                </td>
-                <td class="fonts">
-                    @foreach($detail_post as $values){{ $values->wp_tel }}@endforeach
-                </td>
-            </tr>
-            <tr  class="fonts">
-                <td width="30%" align="right">
-                    Facebook :
-                </td>
-                <td class="fonts">
-                    @foreach($detail_post as $values){{ $values->wp_fb }}@endforeach
-                </td>
-            </tr>
-            <tr  class="fonts">
-                <td width="30%" align="right">
-                    E-mail :
-                </td>
-                <td class="fonts">
-                    @foreach($detail_post as $values){{ $values->wp_email }}@endforeach
-                </td>
-            </tr>
-            <tr  class="fonts">
-                <td width="30%" align="right">
-                    รูปภาพ :
-                </td>
-                <td class="fonts">
-                    @foreach($detail_post as $values)
-                        <img src="{{url('picture/'.$values->wp_pic)}}" alt="รูป ตำแหน่งที่ทำงาน" width="250"
-                             height="180">
-                    @endforeach
-                </td>
-            </tr>
-        </table>
+<div class="modal-body">
+    <div class="container">
+        <div class="row">
+            @foreach($detail_post as $values)
+            <div class="col-xs-2 col-md-2" align="right">
+                <img src="{{url('picture/'.$values->wp_pic)}}" alt="รูป ตำแหน่งที่ทำงาน" width="180" height="150">
+            </div>
+            <div class="col-xs-5 col-sm-5 col-md-5" style="margin-left: -0.5cm;">
+                <ul class="ul-fix">
+                    <li>ชื่อบริษัท/หัวข้อ : {{ $values->wp_titel }}</li>
+                    <li>/</li>
+                    <li>ประเภทงาน : {{ $values->wp_description }}</li>
+                    <br>
+                    <li>จำนวน/ตำแหน่ง  : <b>{{ $values->wp_total }}</b></li>
+                    <li>/</li>
+                    <li>รายละเอียด : {{ $values->wp_detail }}</li>
+                    <br>
+                    <li>สถานที่  : {{ $values->wp_location }}</li>
+                    <li>/</li>
+                    <li>คุณสมบัติ : {{ $values->wp_property }}</li>
+                    <br>
+                    <li>เบอร์  : {{ $values->wp_location }}</li>
+                    <li>/</li>
+                    <li>Facebook : {{ $values->wp_fb }}</li>
+                    <br>
+                    <li>E-mail : {{ $values->wp_email }}</li>
+                    <br>
+                    <li style="margin-left: 1%">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star-half-o"></i>
+                    </li>
+                </ul>
+            </div>
+            @endforeach
+        </div>
     </div>
 </div>
-<div class="modal-footer" style="margin-top: -1cm;">
+<div class="modal-footer">
     <button type="button" class="btn btn-warning btn-sm" data-dismiss="modal"
-            style="font-family: ThaiNeue;font-size: 18pt;">Close</button>
+            style="font-family: ThaiNeue;font-size: 18pt;">Close
+    </button>
 </div>
+
+{{--<div class="table-responsive">--}}
+{{--<table class="table" cellpadding="0" cellspacing="0">--}}
+{{--<tr class="fonts">--}}
+{{--<td width="30%" align="right">ชื่อบริษัท/หัวข้อ :</td>--}}
+{{--<td>--}}
+{{--@foreach($detail_post as $values){{ $values->wp_titel }}@endforeach--}}
+{{--</td>--}}
+{{--</tr>--}}
+{{--<tr class="fonts" >--}}
+{{--<td width="30%" align="right">ประเภทงาน :</td>--}}
+{{--<td class="fonts">--}}
+{{--@foreach($detail_post as $values){{ $values->wp_description }}@endforeach--}}
+{{--</td>--}}
+{{--</tr>--}}
+{{--<tr  class="fonts">--}}
+{{--<td width="30%" align="right">จำนวน/ตำแหน่ง :</td>--}}
+{{--<td class="fonts">--}}
+{{--@foreach($detail_post as $values){{ $values->wp_total }}@endforeach--}}
+{{--</td>--}}
+{{--</tr>--}}
+{{--<tr  class="fonts">--}}
+{{--<td width="30%" align="right">รายละเอียด :</td>--}}
+{{--<td class="fonts">--}}
+{{--@foreach($detail_post as $values){{ $values->wp_detail }}@endforeach--}}
+{{--</td>--}}
+{{--</tr>--}}
+{{--<tr  class="fonts">--}}
+{{--<td width="30%" align="right">สถานที่ :</td>--}}
+{{--<td class="fonts">--}}
+{{--@foreach($detail_post as $values){{ $values->wp_location }}@endforeach--}}
+{{--</td>--}}
+{{--</tr>--}}
+{{--<tr  class="fonts">--}}
+{{--<td width="30%" align="right">คุณสมบัติ :</td>--}}
+{{--<td class="fonts">--}}
+{{--@foreach($detail_post as $values){{ $values->wp_property }}@endforeach--}}
+{{--</td>--}}
+{{--</tr>--}}
+{{--<tr  class="fonts">--}}
+{{--<td width="30%" align="right">เบอร์โทร :--}}
+{{--</td>--}}
+{{--<td class="fonts">--}}
+{{--@foreach($detail_post as $values){{ $values->wp_tel }}@endforeach--}}
+{{--</td>--}}
+{{--</tr>--}}
+{{--<tr  class="fonts">--}}
+{{--<td width="30%" align="right">Facebook :</td>--}}
+{{--<td class="fonts">--}}
+{{--@foreach($detail_post as $values){{ $values->wp_fb }}@endforeach--}}
+{{--</td>--}}
+{{--</tr>--}}
+{{--<tr  class="fonts">--}}
+{{--<td width="30%" align="right">E-mail :</td>--}}
+{{--<td class="fonts">--}}
+{{--@foreach($detail_post as $values){{ $values->wp_email }}@endforeach--}}
+{{--</td>--}}
+{{--</tr>--}}
+{{--<tr  class="fonts">--}}
+{{--<td width="30%" align="right">รูปภาพ :</td>--}}
+{{--<td class="fonts">--}}
+{{--@foreach($detail_post as $values)--}}
+{{--<img src="{{url('picture/'.$values->wp_pic)}}" alt="รูป ตำแหน่งที่ทำงาน" width="250"--}}
+{{--height="180">--}}
+{{--@endforeach--}}
+{{--</td>--}}
+{{--</tr>--}}
+{{--</table>--}}
+{{--</div>--}}
