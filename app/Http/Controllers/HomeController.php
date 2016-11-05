1af4c78adc51cf8c7d0ecf2591609a6fddde6151
slumboy <Taa_wappey@windowslive.com>
2016-11-05 13:01:26 +0700

    Register
    Redirect to page any person

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,13 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        if(Auth::user()->status=='ผู้ว่าจ้าง'){
+            return redirect('/showpostEmployer');
+        }
+        else if(Auth::user()->status=='PartTime'){
+            return redirect('/manageProfile');
+        }else {
+            return redirect('/homepage');
+        }
     }
 }
