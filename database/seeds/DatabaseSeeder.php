dd23b836163461b2cff839cc36df29a2b1198ed4
slumboy <Taa_wappey@windowslive.com>
2016-11-05 14:19:57 +0700

    seed
    insert data
    - catagory
    - location

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -30,20 +30,35 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->delete();
-        DB::table('users')->insert([
-            'std_id'=>'5601111',
-            'user_name'=>'555555',
-            'password'=>'ppppp',
-            'name'=>'xxxx',
-            'lastname'=>'kkkk',
-            'britday'=>'2559-11-10',
-            'sex'=>'1',
-            'email'=>'nnn@gamil.com',
-            'tel'=>'0865214',
-            'address'=>'svfvdxbgfbgfbgcbgc',
-            'facebook'=>'noom',
-            'status'=>'1'
-        ]);
+        DB::table('category')->delete();
+        DB::table('category')
+            ->insert(array(
+                array('c_id'=>'1','c_name'=>'Programming'),
+                array('c_id'=>'2','c_name'=>'ซ่อมคอม'),
+                array('c_id'=>'3','c_name'=>'ตัดต่อวีดีโอ'),
+                array('c_id'=>'4','c_name'=>'ตัดต่อรูปภาพ'),
+                array('c_id'=>'5','c_name'=>'ช่างภาพ'),
+                array('c_id'=>'6','c_name'=>'Event!')
+            ));
+
+//        tb_location
+        DB::table('tb_locations')->delete();
+        DB::table('tb_locations')
+            ->insert(array(
+                array('id'=>'1','location'=>'แกดำ'),
+                array('id'=>'2','location'=>'โกสุมพิสัย'),
+                array('id'=>'3','location'=>'บ้านหนองอุ่ม'),
+                array('id'=>'4','location'=>'กันทรวิชัย'),
+                array('id'=>'5','location'=>'กุดรัง'),
+                array('id'=>'6','location'=>'ชื่นชม'),
+                array('id'=>'7','location'=>'เชียงยืน'),
+                array('id'=>'8','location'=>'นาเชือก'),
+                array('id'=>'9','location'=>'นาดูน'),
+                array('id'=>'10','location'=>'บรบือ'),
+                array('id'=>'11','location'=>'พยัคฆภูมิพิสัย'),
+                array('id'=>'12','location'=>'เมืองมหาสารคาม'),
+                array('id'=>'13','location'=>'ยางสีสุราช'),
+                array('id'=>'14','location'=>'วาปีปทุม')
+            ));
     }
 }
